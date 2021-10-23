ab9a5a22dceac245bd5dbfae96a11c67953f9afe
monsieur486 <monsieur486@gmail.com>
2021-10-23 19:22:03 +0200

    Add dev and prod files porperties

--- src/main/java/com/mr486/starterkit/StarterKitApplication.java
+++ src/main/java/com/mr486/starterkit/StarterKitApplication.java
@@ -1,13 +1,32 @@
 package com.mr486.starterkit;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import javax.annotation.PostConstruct;
+
+
 
 @SpringBootApplication
 public class StarterKitApplication {
-
   public static void main(String[] args) {
     SpringApplication.run(StarterKitApplication.class, args);
+  }
+
+  @Value("${server.port}")
+  Integer portNumber;
+
+  @Value("${spring.profiles.active}")
+  String  profile;
+
+
+  @PostConstruct
+  public void postConstruct() {
+    String message = "Serveur lancé sur le port: " + portNumber.toString() +
+      " avec le profil: " +
+      profile;
+
+    System.out.println(message);
   }
 
 }
