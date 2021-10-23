552c1bfefe9aee812b6b51a34787b16beb4690c0
monsieur486 <monsieur486@gmail.com>
2021-10-23 20:25:39 +0200

    Edit init message

--- src/main/java/com/mr486/starterkit/StarterKitApplication.java
+++ src/main/java/com/mr486/starterkit/StarterKitApplication.java
@@ -1,38 +1,58 @@
 package com.mr486.starterkit;
 
+import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import javax.annotation.PostConstruct;
 
-
-
+/**
+ * Starter kit application.
+ */
 @SpringBootApplication
 public class StarterKitApplication {
+  /**
+   * The entry point of application.
+   *
+   * @param args the input arguments
+   */
   public static void main(String[] args) {
     SpringApplication.run(StarterKitApplication.class, args);
   }
 
+  /**
+   * The Port number.
+   */
   @Value("${server.port}")
   Integer portNumber;
 
+  /**
+   * The Profile.
+   */
   @Value("${spring.profiles.active}")
   String  profile;
 
 
+  /**
+   * Post construct.
+   */
   @PostConstruct
   public void postConstruct() {
+    StringBuilder message = getInitMessage();
+
+    System.out.println(message);
+  }
+
+  private StringBuilder getInitMessage() {
     StringBuilder message = new StringBuilder("Serveur lancé\nPort d'écoute: ");
     message.append(portNumber.toString()).append("\n");
-    if(profile.equals("dev")){
+    if (profile.equals("dev")) {
       message.append("Mode développement");
-    } else if (profile.equals("prod")){
+    } else if (profile.equals("prod")) {
       message.append("Mode production");
     } else {
       message.append("Mode inconnu");
     }
-
-    System.out.println( message.toString());
+    return message;
   }
 
 }
