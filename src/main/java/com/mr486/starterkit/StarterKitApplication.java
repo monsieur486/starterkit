69bb2482d52ba8e0bd0a3497fa3f8e49f51b1827
monsieur486 <monsieur486@gmail.com>
2021-10-23 18:52:37 +0200

    first commit

---
+++ src/main/java/com/mr486/starterkit/StarterKitApplication.java
@@ -0,0 +1,13 @@
+package com.mr486.starterkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterKitApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(StarterKitApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/mr486/starterkit/StarterKitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mr486.starterkit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterKitApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
